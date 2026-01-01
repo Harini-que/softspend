3fdbd0946b7a7cf7a6dea2cd0a84674faa74be58
Harini-que <srinivasanharini91@gmail.com>
2026-01-01 23:41:51 +0530

    Add service worker for file caching
    Implement service worker for caching files.

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "softspend-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
